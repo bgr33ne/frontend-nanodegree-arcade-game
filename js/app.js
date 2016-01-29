20ada3df0e78f5d2c95a49f67e8c1a04c44b61cf
Brian Greene <bgreene623@gmail.com>
2016-01-29 11:12:43 -0500

    App Working
    Placed players + enemies on field. Added collision detection. Added map
    boundaries. Add competition and reset functions.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.x = this.x + this.speed * dt;
+    if (this.x > 505) {
+        this.x = 0;
+    }
+    return this;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -24,13 +32,79 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
+
+//allows update without letting player over the borders of map
+Player.prototype.update = function(dt) {
+    if (this.collide() === true) {
+            console.log("Reset!");
+            this.reset();
+    } 
+
+    if (this.x < 0) {
+            this.x = 0;
+        } else if (this.x > 400) {
+            this.x = 400;
+        } else if (this.y < 100) {
+            this.y = 100;
+            console.log("You win!");
+            this.reset(); //moves player to back to beginning
+        } else if (this.y < 0) {
+            this.y = 0;
+        } else if (this.y > 400) {
+            this.y = 400;
+        }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+            case 'left': 
+                this.x = this.x - 100;
+                break;
+            case 'up':
+                this.y = this.y - 100;
+                break;
+            case 'right':
+                this.x = this.x + 100;
+                break;
+            case 'down':
+                this.y = this.y + 100;
+                break;
+        }
+};
+
+//collision function working now
+Player.prototype.collide = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (this.x > allEnemies[i].x - 75 &&
+            this.x < allEnemies[i].x + 75 &&
+            this.y > allEnemies[i].y - 50 &&
+            this.y < allEnemies[i].y + 50) {
+            console.log('Reset!!');
+            return true;
+        }
+    }
+};
+//moves player to beginning
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [new Enemy(450, 200, 200), new Enemy(200, 300, 150), new Enemy(150, 200, 200), new Enemy(100, 100, 100)];
+var player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
